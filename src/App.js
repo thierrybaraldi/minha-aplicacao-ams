aa69a32631b2a8ecaf02b6b528cb5cff4374965c
thierrybaraldi <20231350327@fatec-id>
2025-10-20 17:41:02 -0300

    dkajdkaj

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+     <h1>Site em React</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Componentes/Navbar/Nav.js
@@ -0,0 +1,11 @@
+import './Nav.css'
+
+function Nav() {
+  return (
+    <nav style={{ textAlign: 'center' }}>
+        
+    </nav>
+  );
+}
+
+export default Nav;
